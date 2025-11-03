cfd6290b5a461f66759cf0a27df2e216e04f56b6
Anish Karki <anish.karki1.618@gmail.com>
2025-11-03 22:00:35 +1000

    chagens

--- Scripts/opensearchcontroller.py
+++ Scripts/opensearchcontroller.py
@@ -1,4 +1,5 @@
 from opensearchpy import OpenSearch 
+import json
 
 host = 'localhost'
 port = 19200
@@ -13,13 +14,34 @@ client = OpenSearch(
     ssl_asserts_hostname=False,
     ssl_show_warn=False
 )
-print(client.info())
 
-metadata = client.cluster.state(metric='metadata')
+# View all the details about the indices
+def get_index_metainfo(client=None, index=None, format='json'):
+    return client.cat.indices(index=index, format=format)
 
-for index_name in metadata['metadata']['indices']:
-    print(index_name)
+# view index mapping and settings
+def get_index_mapandSettings(client=None, index=None):
+    respose =  client.indices.get(index=index)
+    return json.dumps(respose)
 
-for idx in client.indices.get_alias("*"):
-    stats = client.indices.stats(index=idx)
-    print(f"{idx}: docs={stats['_all']['primaries']['docs']['count']}")
+def get_logs_orderbydate(client=None, index=None, size=0):
+    query = {
+        "size":size,
+        "sort": [{"@timestamp": {"order": "desc"}}],
+        "query": {"match_all": {}},
+        "_source": ["@timestamp", "_raw", "level"]
+    }
+    resp = client.search(index=index, body=query)
+    for hit in resp['hits']['hits']:
+        raw_log = hit['_source'].get('_raw', hit['_source'].get('message', 'N/A'))
+        ts = hit['_source']['@timestamp']
+        print(f"[{ts}] Full log:\n {raw_log}\n {'-'*80}")
+
+
+info = get_index_metainfo(client, index='postgreslogs', format='json')
+print(info[0])
+
+respose = get_index_mapandSettings(client, index='postgreslogs')
+print(respose)
+
+get_logs_orderbydate(client, index='postgreslogs', size=100)
